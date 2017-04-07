2ded1dcf1e8d842cf83b4dcf659e4ffde4d0c4bf
xi1zhang <kevin.zhang@nokia.com>
2017-04-07 14:55:47 +0800

    add

---
+++ src/com/zero4kevin/Main.java
@@ -0,0 +1,8 @@
+package com.zero4kevin;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/
+    }
+}
---
+++ src/com/zero4kevin/StringQuestions.java
@@ -0,0 +1,103 @@
+package com.zero4kevin;
+
+/**
+ * Created by xi1zhang on 2017/4/7.
+ */
+public class StringQuestions {
+    private static String (String args[]) throws Exception{
+        if (args[] !=null){
+            return args[];
+        }else{
+            throw new Exception("parameter is empty");
+        }
+    }
+
+    /**
+     * Writing a java program to print duplicate characters from String
+     * @param s
+     */
+    public void printDuplicateCharacters(String s){
+
+    }
+
+    /**
+     * Wrting a Java program to check two strings are anagrams or not
+     * @param s
+     */
+    public void checkTwoStringsAnagrams(String s){
+
+    }
+
+    /**
+     * Writing a java program to count the number of words in a string
+     */
+    public void countNumberOfWords(String s){
+
+    }
+
+    /**
+    *Writing a java program to count the total number of occurrences of a given character in a string without using any loop
+     */
+    public void countNumberOfOccurrencesOfCharacterNoLoop(String s){
+
+    }
+
+    /**
+     *
+     * @param s
+     */
+    public void reverseEachWord(String s){
+
+    }
+
+    /**
+     *Writing a java program to reverse a string
+     */
+    public void reverseString(String s){
+
+    }
+
+    /**
+     *remove all white spaces from a string
+     */
+    public void removeWhiteSpaces(String s){
+
+    }
+
+    /*
+    print first non repeated character from a string
+     */
+    public void printRepeatedCharacter(String s){
+
+    }
+
+    /**
+     *  reverse String by iteration and recursion
+     * @param
+     */
+    public void reverseStringIterationaRecurion (String s){
+
+    }
+
+    /**
+     * check if a string contains only digits
+     * @param s
+     */
+    public void isContainOnlyDigits(String s){
+
+    }
+    /**
+     * reverse a given string with preserving the position of spacess
+     */
+    public void reverseStringPreservionPositionOfSpace(String s){
+
+    }
+
+    /**
+     *
+     * @param args
+     */
+    public static void main (String args[]){
+
+    }
+}
