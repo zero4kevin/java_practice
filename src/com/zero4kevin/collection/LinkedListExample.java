5cfe67e9bfacdd42a1208723d61927981b224471
zero4kevin <kevin21th@hotmail.com>
2017-04-26 22:29:50 +0800

    add collection examples

---
+++ src/com/zero4kevin/collection/LinkedListExample.java
@@ -0,0 +1,13 @@
+package com.zero4kevin.collection;
+
+import java.util.LinkedList;
+
+/**
+ * Created by kevin on 4/26/17.
+ */
+public class LinkedListExample {
+    public static void main(String[] args){
+        LinkedList<String> linkedList=new LinkedList<String>();
+
+    }
+}
